57ea9021f6106a7857a3440d38f02c2cf0046c58
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-03-17 18:34:14 -0300

    update

--- srcBack/backend-produtos/main.py
+++ srcBack/backend-produtos/main.py
@@ -2,18 +2,16 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# 1. Configurando a conexão com o MySQL
-# Formato: mysql+pymysql://usuario:senha@servidor:porta/nome_do_banco
-URL_BANCO_DADOS = "mysql+pymysql://root:arth03092002@@localhost:3306/feira_virtual"
+import urllib.parse 
+senha = urllib.parse.quote_plus("arth03092002@") 
+URL_BANCO_DADOS = f"mysql+pymysql://root:{senha}@localhost:3306/feira_virtual"
 
-# Criando o motor de conexão (sem as travas do SQLite)
 engine = create_engine(URL_BANCO_DADOS)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# 2. A "Mágica" do ORM: Transformando Classe em Tabela SQL
 class Produto(Base):
-    __tablename__ = "produtos" # Nome da tabela no MySQL
+    __tablename__ = "produtos" 
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String, index=True)
@@ -23,10 +21,8 @@ class Produto(Base):
     descricao = Column(String)
     imagem_url = Column(String)
 
-# 3. Comando que avisa o Python para ir no MySQL e criar as tabelas
 Base.metadata.create_all(bind=engine)
 
-# 4. Iniciando a nossa API
 app = FastAPI(title="API Feira Virtual - Produtos")
 
 @app.get("/")
